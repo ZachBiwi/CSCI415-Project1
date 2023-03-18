94f383b20dec793f5e6fa99a43737b9e6054dca3
Nathan O'connor <nate600@gmail.com>
2023-03-18 18:45:36 -0500

    Final Project

--- Keys.cpp
+++ Keys.cpp
@@ -4,7 +4,7 @@
 *   Authors: Nathan O'Connor, Zachary Sawicki, Joseph Hoversten, Jacob Haapoja
 *   Class: CSCI 415
 *
-*   ©2023
+*   ï¿½2023
 ***************************************************************************************/
 
 #include "Keys.h"
@@ -13,9 +13,9 @@ using namespace std;
 
 /**
 *@brief This is the default constructor
-*@param n1
-*@param e1
-*@param d1
+*@param n1 the n value for the key pair
+*@param e1 the e value for the public key
+*@param d1 the d value for the private key
 */
 Keys::Keys(InfInt n1, InfInt e1, InfInt d1) {
     n = n1;
